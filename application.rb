87c66790bfdf269cb6ea3238cae3a738a9aa4b72
dzucconi <mail@damonzucconi.com>
2014-12-04 21:12:38 -0500

    Clean up

--- application.rb
+++ application.rb
@@ -4,12 +4,13 @@ class HTML
   end
 
   def tag(type, attributes, content)
-    attributes
-      .map { |k, v| "#{k}='#{v}'" }.join(' ')
-      .let { |attrs| " #{attrs}" unless attrs.empty? }
-      .let do |attrs|
-        content.nil? ? "<#{type}#{attrs}>" : "<#{type}#{attrs}>#{content}</#{type}>"
-      end
+    attributes.map { |k, v|
+      "#{k}='#{v}'"
+    }.join(' ').let { |attrs|
+      " #{attrs}" unless attrs.empty?
+    }.let { |attrs|
+      content.nil? ? "<#{type}#{attrs}>" : "<#{type}#{attrs}>#{content}</#{type}>"
+    }
   end
 
   def page
