12350194a32a6e9215046f913b69b5f4174a358d
dzucconi <mail@damonzucconi.com>
2015-07-03 12:25:57 -0400

    Mobile optimizations

--- lib/html.rb
+++ lib/html.rb
@@ -21,6 +21,9 @@ class HTML
       head {
         meta(charset: 'utf-8') +
         meta('http-equiv' => 'X-UA-Compatible', content: 'IE=edge,chrome=1') +
+        meta(name: 'viewport', content: 'width=device-width') +
+        meta(name: 'apple-mobile-web-app-capable', content: 'yes') +
+        meta(name: 'apple-mobile-web-app-status-bar-style', content: 'black') +
         title { 'Slow Verb' } +
         link(rel: 'stylesheet', type: 'text/css', href: stylesheet_path('application')) +
         script(src: javascript_path('application'), type: 'text/javascript') {}
